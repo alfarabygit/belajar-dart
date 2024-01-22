2f56cfc61441e360dadf0de0067bfd93ccf2b330
Alfaraby Indra <alfarabyindra@gmail.com>
2024-01-22 21:39:48 +0700

    upload

---
+++ dart_dasar1/bin/dart_dasar1.dart
@@ -0,0 +1,4 @@
+//sintaks pemrograman dart sederhana
+void main(List<String> arguments) {
+  print("Hello Everyone, Welcome to Chaos World 2024!");
+}
---
+++ dart_dasar2/bin/dart_dasar2.dart
@@ -0,0 +1,10 @@
+//input output pemrograman dart
+
+import 'dart:io'; //package utk kode input output dart
+
+void main(List<String> arguments) {
+  String? input;
+  input = stdin.readLineSync();
+
+  print(input);
+}
